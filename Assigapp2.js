a8b976a332cc7767342b67bf956972fe1f4560eb
JimmyHH17 <Hazim.hazimin@gmail.com>
2020-06-12 14:33:19 +0800

    Updated

--- Assigapp2.js
+++ Assigapp2.js
@@ -6,13 +6,13 @@ const app = express();
 
 app.use('/user', (req, res)=>{
 
-  res.send('<h1>This is the user page!!!!</h1>');
+  res.send('<h1>This is jimmy page!!!!</h1>');
 
 });
 
 app.use('/', (req, res)=>{
 
-  res.send('<h1>Welcomeeee!!!!</h1>');
+  res.send('<h1>Welcomeeee & thank you for visiting!!!!</h1>');
 
 });
 
